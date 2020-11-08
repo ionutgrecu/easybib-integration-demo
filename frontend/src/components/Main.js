6993270ea4bc03153d02887ddaba4e7a2f508fff
Ionut Grecu <ionut@grecu.eu>
2020-11-08 22:04:34 +0200

    url start fix

---
+++ frontend/src/components/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DropboxChooser from "react-dropbox-chooser";
+import { Container, Row, Col, Table } from "react-bootstrap";
+
+class Main extends React.Component {
+    constructor() {
+        super()
+    }
+    
+    render() {
+        return 'xx'
+    }
+} export default Main
