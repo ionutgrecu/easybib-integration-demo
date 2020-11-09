f40903f0f3d697349fb0bb68c0c04bbeb97d2bad
Ionut Grecu <ionut@grecu.eu>
2020-11-09 08:47:06 +0200

    publication store

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,13 +1,54 @@
 import React from "react";
-import DropboxChooser from "react-dropbox-chooser";
-import { Container, Row, Col, Table } from "react-bootstrap";
+import { Container, Row, Col, Table } from "react-bootstrap"
+import PublicationStore from '../stores/PublicationStore'
 
 class Main extends React.Component {
     constructor() {
         super()
+
+        this.state = {
+            publications: [],
+        }
+
+        this.store = new PublicationStore()
     }
-    
+
+    componentDidMount() {
+        this.store.emitter.addListener('GET_PUBLICATIONS_SUCCESS', () => {
+            this.setState({
+                publications: this.store.publications
+            })
+        })
+
+        this.store.getPublications()
+    }
+
     render() {
-        return 'xx'
+        return (
+            <>
+                <Container>
+                    <Row>
+                        <Col>
+                            <Table striped bordered>
+                                <thead>
+                                    <tr>
+                                    <th>ID</th>
+                                    <th>Title</th>
+                                    <th>Type</th>
+                                    <th>Publish Date</th>
+                                    <th>&nbsp;</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {this.state.store.publications.map(e)=>(
+                                        
+                                    )}
+                                </tbody>
+                            </Table>
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+        )
     }
 } export default Main
--- frontend/src/stores/PublicationStore.js
+++ frontend/src/stores/PublicationStore.js
@@ -17,4 +17,4 @@ class PublicationStore {
             this.emitter.emit('GET_PUBLICATIONS_SUCCESS')
         }
     }
-}
+} export default PublicationStore
