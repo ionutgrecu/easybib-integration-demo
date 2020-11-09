42f1fa4cf1274afa7597eddd29b7dbd09f33f242
Ionut Grecu <ionut@grecu.eu>
2020-11-09 08:35:33 +0200

    publications store

---
+++ frontend/src/stores/PublicationStore.js
@@ -0,0 +1,20 @@
+import { EventEmitter } from 'fbemitter'
+import axios from 'axios'
+const SERVER_URL = 'http://localhost:8080'
+
+class PublicationStore {
+    constructor() {
+        this.publications = []
+        this.emitter = new EventEmitter()
+    }
+
+    async getPublications(){
+        try {
+            const response=await axios.get(`${SERVER_URL}/publications`)
+            this.documents=response.data
+            this.emitter.emit('GET_PUBLICATIONS_SUCCESS')
+        } catch (error) {
+            this.emitter.emit('GET_PUBLICATIONS_SUCCESS')
+        }
+    }
+}
