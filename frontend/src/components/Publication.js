24ef22b2531335c3e8fdcd9a6c3f03c4e96514ed
Ionut Grecu <ionut@grecu.eu>
2020-11-09 20:54:56 +0200

    authors list

--- frontend/src/components/Publication.js
+++ frontend/src/components/Publication.js
@@ -1,25 +1,38 @@
 import React from "react";
 
-class Publication extends React.Component{
-    constructor(props){
+class Publication extends React.Component {
+    constructor(props) {
         super(props)
 
-        this.state={
-            title:this.props.item.title
+        this.state = {
+            title: this.props.item.title
         }
     }
 
-    render(){
+    render() {
         const { item } = this.props
-console.log(item)
+
         return (
             <>
-            <tr>
-                <td>{item.id}</td>
-                <td>{item.title}</td>
-                <td>{item.type}</td>
-                <td>{item.publishedAt}</td>
-            </tr>
+                <tr>
+                    <td>{item.id}</td>
+                    <td>{item.title}<br /><small><strong>Authors:</strong>
+                        {item.authors.map((i) =>
+                            <>
+                                <span>{i.name}</span>, &nbsp;
+                            </>
+                        )}
+                    </small></td>
+                    <td>{item.type}</td>
+                    <td>{item.publishedAt}</td>
+                    <td><a className="btn btn-info" title="Edit">
+                        <i className="fas fa-pencil-alt" onClick={this.edit}></i>
+                    </a>{" "}
+                        <a className="btn btn-danger" title="Delete" onClick={this.delete}>
+                            <i className="fas fa-trash-alt"></i>
+                        </a>{" "}
+                    </td>
+                </tr>
             </>
         )
     }
