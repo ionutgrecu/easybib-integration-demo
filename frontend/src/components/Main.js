82374d4dee1da9aee7259924dbf3e61f6b79be1c
Ionut Grecu <ionut@grecu.eu>
2020-11-09 21:36:52 +0200

    publicationForm

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Container, Row, Col, Table } from "react-bootstrap"
 import PublicationStore from '../stores/PublicationStore'
 import Publication from './Publication'
+import PublicationForm from './PublicationForm'
 
 class Main extends React.Component {
     constructor() {
@@ -9,9 +10,16 @@ class Main extends React.Component {
 
         this.state = {
             publications: [],
+            isEditing: false
         }
 
         this.store = new PublicationStore()
+
+        this.showForm = () => {
+            this.setState({
+                isEditing: true
+            })
+        }
     }
 
     componentDidMount() {
@@ -25,31 +33,46 @@ class Main extends React.Component {
     }
 
     render() {
-        return (
-            <>
-                <Container>
-                    <Row>
-                        <Col>
-                            <Table striped bordered>
-                                <thead>
-                                    <tr>
-                                        <th>ID</th>
-                                        <th>Title</th>
-                                        <th>Type</th>
-                                        <th>Publish Date</th>
-                                        <th>&nbsp;</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {
-                                        this.state.publications.map((e) => <Publication key={e.id} item={e} />)
-                                    }
-                                </tbody>
-                            </Table>
-                        </Col>
-                    </Row>
-                </Container>
-            </>
-        )
+        if (this.state.isEditing === false)
+            return (
+                <>
+                    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarNav">
+                            <ul className="navbar-nav">
+                                <li className="nav-item">
+                                    <a className="nav-link" onClick={this.showForm}><i className="fas fa-plus"></i> Add Publication</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                    <Container className="mt-3">
+                        <Row>
+                            <Col>
+                                <Table striped bordered>
+                                    <thead>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Title</th>
+                                            <th>Type</th>
+                                            <th>Publish Date</th>
+                                            <th>&nbsp;</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {
+                                            this.state.publications.map((e) => <Publication key={e.id} item={e} />)
+                                        }
+                                    </tbody>
+                                </Table>
+                            </Col>
+                        </Row>
+                    </Container>
+                </>
+            )
+        else
+            return <PublicationForm />
     }
 } export default Main
---
+++ frontend/src/components/PublicationForm.js
@@ -0,0 +1,68 @@
+import React from "react"
+import { Container, Row, Col, Button, Form } from "react-bootstrap"
+import DatePicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css"
+import { format } from "date-fns"
+
+class PublicationForm extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            title: "",
+            type: "other",
+            publishedAt: new Date()
+        }
+
+        this.handleChange = (e) => {
+            // if (e.target.type == 'text' || e.target.type == 'select-one')
+            this.setState({ [e.target.name]: e.target.value })
+        }
+
+        this.dateChange = (e) => {
+            this.setState({ publishedAt: e })
+            this.publishedAt = format(e, 'yyyy-MM-dd')
+        }
+    }
+
+    render() {
+        return (
+            <Container>
+                <Row>
+                    <Col>
+                        <Form>
+                            <Form.Group>
+                                <Form.Label>Publication title</Form.Label>
+                                <Form.Control type="text" name="title" placeholder="Ex: Lorem ipsum dolor sit amet" onChange={this.handleChange} />
+                            </Form.Group>
+
+                            <Form.Group>
+                                <Form.Label>Publication type</Form.Label>
+                                <Form.Control as="select" name="type" onChange={this.handleChange}>
+                                    <option>book</option>
+                                    <option>newspaper</option>
+                                    <option>paperwork</option>
+                                    <option>other</option>
+                                </Form.Control>
+                            </Form.Group>
+
+                            <Form.Group>
+                                <Form.Label>Publication date</Form.Label>
+                                <DatePicker name="publishedAt" dateFormat="yyyy-MM-dd" onChange={this.dateChange} />
+                            </Form.Group>
+
+                            <Form.Row>
+                                <Button variant="primary" type="submit">
+                                    Save
+                                </Button>
+                                <Button variant="danger" type="button">
+                                    Cancel
+                                </Button>
+                            </Form.Row>
+                        </Form>
+                    </Col>
+                </Row>
+            </Container>
+        )
+    }
+} export default PublicationForm
